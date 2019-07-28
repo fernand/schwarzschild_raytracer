1dbebb02d5978d602bfee2903d1d3c1c1e4fc78a
Fernand Pajot <fernand@epigram.me>
2019-07-28 08:39:44 -0700

    No longer removing all pdb files.

--- build.bat
+++ build.bat
@@ -3,8 +3,6 @@
 IF NOT EXIST build mkdir build
 pushd build
 
-rem del *.pdb > NUL 2> NUL
-
 set compilerFlags=-Z7 -FC -wd4005 /MP6
 cl /nologo %compilerFlags% ..\main.c ..\include\glad\glad.c /link ..\glfw3dll.lib
 
