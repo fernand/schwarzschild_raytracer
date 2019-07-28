23b30e590cb2ff326fb737afdbaec438d4950365
Fernand Pajot <fernand@epigram.me>
2019-07-28 08:31:12 -0700

    Multiprocess build.

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ pushd build
 
 rem del *.pdb > NUL 2> NUL
 
-set compilerFlags=-Z7 -FC -wd4005
+set compilerFlags=-Z7 -FC -wd4005 /MP6
 cl /nologo %compilerFlags% ..\main.c ..\include\glad\glad.c /link ..\glfw3dll.lib
 
 popd
